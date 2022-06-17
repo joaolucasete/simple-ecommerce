431ca67dbd00d1396bf2662095114b6845e30ba9
matheusmppaiva <matheus.paiva@gec.inatel.br>
2022-06-17 14:57:55 -0300

    Adding classes Produto and Estoque

--- ecommerce/src/main/java/Estoque.java
+++ ecommerce/src/main/java/Estoque.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Estoque {
+
+    List<Produto> produtos = new ArrayList<Produto>();
+
+    public void cadastraProduto(Produto produto, int quantidade)
+    {
+        boolean achou = false;
+        for(int i = 0; i< produtos.size(); i++){
+            if(produtos.get(i).nome == produto.getNome())
+            {
+                achou = true;
+                produto.quantidade += quantidade;
+            }
+        }
+        if(!achou){
+            produtos.add(produto);
+        }
+    }
+    public void retiraProduto(Produto produto, int quantidade)
+    {
+        boolean achou = false;
+        for(int i = 0; i< produtos.size(); i++){
+            if(produtos.get(i).nome == produto.getNome())
+            {
+                achou = true;
+                if(produto.quantidade > 1)
+                {
+                    produto.quantidade -= quantidade;
+                }
+                else
+                {
+                    produtos.remove(produto);
+                }
+            }
+        }
+    }
 }
--- ecommerce/src/main/java/Produto.java
+++ ecommerce/src/main/java/Produto.java
@@ -1,2 +1,28 @@
 public class Produto {
+
+    protected String nome;
+    protected double valor;
+    protected int quantidade=0;
+
+    public Produto(String nome,double valor)
+    {
+        this.nome = nome;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 }
